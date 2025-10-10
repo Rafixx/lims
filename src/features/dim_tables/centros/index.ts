08786540e1c70d26765137af3f0a109950b7f910
Rafa <serra.raf@gmail.com>
2025-10-10 10:19:32 +0200

    feat: Eliminar exportación del componente CentroCard en el índice de centros

--- src/features/dim_tables/centros/index.ts
+++ src/features/dim_tables/centros/index.ts
@@ -1,4 +1,3 @@
 export { CentroForm } from './components/CentroForm'
-export { CentroCard } from './components/CentroCard'
 export { CentrosPage } from './pages/CentrosPage'
 export { CreateCentroPage } from './pages/CreateCentroPage'
